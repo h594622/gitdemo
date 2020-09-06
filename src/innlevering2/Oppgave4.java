56ed6fb7038e1631ecec3abe49026cbc113ee093
trudesvardal <trudesvardal@192.168.80.115>
2020-09-06 21:49:56 +0200

    Endret kode etter misforståelse av oppgaven

--- src/innlevering2/Oppgave4.java
+++ src/innlevering2/Oppgave4.java
@@ -1,24 +1,25 @@
 package innlevering2;
 
 import static javax.swing.JOptionPane.*;
-import static java.lang.Integer.*;
+import static java.lang.Double.*;
 
 public class Oppgave4 {
 
 	public static void main(String[] args) {
 	
 		String inntektTxt = showInputDialog ("Inntekt : ");
-		int inntekt = parseInt(inntektTxt);
-	
-		String skatt = "En inntekt på " + inntekt + " ligger på skattetrinn ";
+		double inntekt = parseDouble(inntektTxt);
 		
-		if (inntekt >= 934051) skatt += "4, som tilsvarer 14,52%";
-		else if (inntekt <= 934050 && inntekt >= 580651) skatt += "3, som tilsvarer 11,52%";
-		else if (inntekt <= 580650 && inntekt >= 230951) skatt += "2, som tilsvarer 2,41%";
-		else if (inntekt <= 230950 && inntekt >= 164101) skatt += "1, som tilsvarer 0,93%";
-		else if (inntekt <= 164100) skatt += "0, som tilsvarer 0,00%";
+		String skatt = "En inntekt på " + inntekt + " skal betale en trinnskatt på ";
+	
+		if (inntekt >= 934051) skatt += "kr " + (inntekt*14.52)/100.0;
+		else if (inntekt <= 934050 && inntekt >= 580651) skatt += "kr " + (inntekt*11.52)/100.0;
+		else if (inntekt <= 580650 && inntekt >= 230951) skatt += "kr " + (inntekt*2.41)/100.0;
+		else if (inntekt <= 230950 && inntekt >= 164101) skatt += "kr " + (inntekt*0.93)/100.0;
+		else if (inntekt <= 164100) skatt += "kr " + (inntekt*0.00)/100.0;
 		else skatt += "Ugyldig inntekt";
-
+		
+		
 		showMessageDialog(null, skatt);
 	}
 
